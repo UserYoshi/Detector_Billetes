2cdb88f38858a3f6bb18a72f49fbc82338890d2f
Yoshi <129009389+UserYoshi@users.noreply.github.com>
2025-06-08 18:38:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import torch
 print("✅ GPU disponible:", torch.cuda.is_available())
 
 # Cargar el modelo YOLO personalizado
-model = YOLO("best(1).pt")
+model = YOLO("best.pt")
 
 # Iniciar cámara
 cap = cv2.VideoCapture(0)
